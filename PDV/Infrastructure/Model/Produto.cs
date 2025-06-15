8688610ab240a27c6b5af22aaed2051f3f4d9a10
Cocabele <santiagopacheco34@gmail.com>
2025-06-14 22:46:59 -0300

    atualizando classe produto

--- PDV/Infrastructure/Model/Produto.cs
+++ PDV/Infrastructure/Model/Produto.cs
@@ -1,6 +1,12 @@
-﻿namespace PDV.Infrastructure.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PDV.Infrastructure.Model
 {
     public class Produto
     {
+       public int Id { get; set; }
+       public string Nome { get; set; }
+       public string Descricao { get; set; }
+       public double Custo { get; set; }
     }
 }
