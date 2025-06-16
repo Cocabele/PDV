2346d4a525abdf57b2df392910c45a5f058a2355
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-06-15 22:52:37 -0300

    feat: criado repository para consulta no banco

--- PDV/Infrastructure/Interfaces/IRepository.cs
+++ PDV/Infrastructure/Interfaces/IRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace PDV.Infrastructure.Interfaces;
 
-public interface IRepository
+public interface IRepository<T> where T : class
 {
+    void Create(T obj);
+    void Update(T obj);
+    void Delete(T obj);
+    IEnumerable<T> GetAll();
+    T GetById(int id);
+    T GetByName(string nome);
     
 }
--- PDV/Infrastructure/Repositories/Repository.cs
+++ PDV/Infrastructure/Repositories/Repository.cs
@@ -1,6 +1,36 @@
-﻿namespace PDV.Infrastructure.Repositories;
+﻿using PDV.Infrastructure.Interfaces;
 
-public class Repository
+namespace PDV.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
 {
-    
+    public void Create(T obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(T obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetByName(string nome)
+    {
+        throw new NotImplementedException();
+    }
 }
