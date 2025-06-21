ef783379c500f85704089bf1bfdc0eafb2cbcee3
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-06-20 22:01:25 -0300

    feat: adicionando banco de dados

--- PDV/Program.cs
+++ PDV/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddAuthentication(options =>
 // TODO: alterar banco de dados para SqLite
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
