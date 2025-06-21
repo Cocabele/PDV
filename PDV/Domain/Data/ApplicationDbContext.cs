68a13a51a0ee070f5802b63954c7cf8bea960981
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-06-20 21:18:16 -0300

    feat: criado dbset no appdbcontext

--- PDV/Domain/Data/ApplicationDbContext.cs
+++ PDV/Domain/Data/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PDV.Infrastructure.Model;
 
 namespace PDV.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Produto> Produtos { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
