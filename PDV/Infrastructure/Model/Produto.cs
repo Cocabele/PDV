74e3f7b48620bddf2784011c232379d25f164870
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-06-20 21:17:55 -0300

    feat: incluido novos campos na classe produtos

--- PDV/Infrastructure/Model/Produto.cs
+++ PDV/Infrastructure/Model/Produto.cs
@@ -4,9 +4,12 @@ namespace PDV.Infrastructure.Model
 {
     public class Produto
     {
-       public int Id { get; set; }
-       public string Nome { get; set; }
-       public string Descricao { get; set; }
-       public double Custo { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double? Custo { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public byte[]? Imagem { get; set; }
     }
 }
